f2976a9a763da9b9fd23174b56c54b96ee2263c2
Hexo <91899873+Hexo3@users.noreply.github.com>
2025-03-21 09:21:15 +0200

    delete vars

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,5 @@
 variable "ssh_key" {
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYRwi7Z7OQPLPrjkdfPk0oV976CYNDNj5SMblKy/hjK sysadmin@A326-02"
+  default = ""
 }
 variable "proxmox_host" {
   default = "tiim1"
@@ -9,12 +9,12 @@ variable "template_id" {
 }
 
 # variable "virtual_environment_api_token" {
-#     default = "root@pam!terraform=59f95109-8f32-4973-8b9b-b1147af6fd31"
+#     default = ""
 # }
 variable "proxmox_password" {
-  default = "Par240XXX"
+  default = ""
 }
 
 variable "proxmox_endpoint" {
-    default = "https://192.168.111.161:8006/api2/json"
+    default = ""
 }
